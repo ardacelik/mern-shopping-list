9438ce5462f695f35f6e1dd2043766f18a2f946e
Arda Celik <arda.celik@uoit.net>
2019-07-12 23:11:57 -0400

    Conflict resolved

--- server.js
+++ server.js
@@ -6,3 +6,22 @@ const app = express();
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
+<<<<<<< HEAD
+=======
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    // The next line is added to avoid a warning message on the console
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+>>>>>>> Initial commit
